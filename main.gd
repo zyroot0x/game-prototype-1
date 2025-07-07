8d5ca4b2a086cf7026e4ad706a5fe2c5df899fd9
Diego Emanuel <emanuel.diegorl@gmail.com>
2025-07-07 12:31:34 -0300

    Better optimization

--- main.gd
+++ main.gd
@@ -7,12 +7,22 @@ var enemies: int = 0
 func _ready() -> void:
 	$Timer.start()
 
+# criação de inimigos
 func _on_timer_timeout() -> void:
 	var enemy = enemy_scene.instantiate()
-	enemy.position = Vector2(randi() % 300, randi() % 300)
-	$Enemy.add_child(enemy)
-	enemy_created.emit()
+	var viewport_rect = get_viewport_rect()
+	
+	# gera coordenadas aleatórias dentro do viewport
+	var x = randf_range(0, viewport_rect.size.x)
+	var y = randf_range(0, viewport_rect.size.y)
+	
+	# cria os inimigos
+	enemy.position = Vector2(x, y)
+	$EnemyContainer.add_child(enemy)
 	enemies += 1
+	enemy_created.emit()
+	print("Inimigo criado em: ", enemy.position)
+
 
 func _on_enemy_created() -> void:
-	$Ui/EnemyCounter/MarginContainer/Label.text = "Enemies: " + str(enemies)
+	$Ui/EnemyCounter/MarginContainer/Label.text = "Enemies created: " + str(enemies)
--- scenes/enemy/enemy.gd
+++ scenes/enemy/enemy.gd
@@ -1,1 +1,13 @@
 extends CharacterBody2D
+
+var player = null
+const SPEED = 80
+
+func _ready():
+	player = get_node("/root/Main/Player")
+
+func _physics_process(_delta):
+	if player:
+		var direction = (player.position - position).normalized()
+		velocity = direction * SPEED
+		move_and_slide()
